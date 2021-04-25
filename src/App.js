e49c6e30bc0599ad39a8404b7d35d3133aabfc74
sulman147 <msulman147@gmail.com>
2021-04-25 20:01:14 +0500

    adding login logout system with redux by using google auth through firebase

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+//Pages or Component
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
 import Home from "./pages/Home";
+import ForgotPassword from "./pages/auth/ForgotPassword";
 import Header from "./components/nav/Header";
 import RegisterComplete from "./pages/auth/RegisterComplete";
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+
+  //to check firebase auth state
+  useEffect(() => {
+    const unsubcribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        console.log("user", user);
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+    return () => unsubcribe();
+  }, []);
+
   return (
     <>
       <Header />
       <ToastContainer />
       <Switch>
-        <Route path="/" component={Home} exact={true} />
-        <Route path="/register" component={Register} exact={true} />
-        <Route
-          path="/Register/complete"
-          component={RegisterComplete}
-          exact={true}
-        />
-        <Route path="/login" component={Login} exact={true} />
+        <Route path="/" component={Home} exact />
+        <Route path="/login" component={Login} exact />
+        <Route path="/register" component={Register} exact />
+        <Route path="/Register/complete" component={RegisterComplete} exact />
+        <Route path="/forgot/password" component={ForgotPassword} exact />
       </Switch>
     </>
   );
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,9 +1,131 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { auth, googleAuthProvider } from "../../firebase";
+import { toast } from "react-toastify";
+import { Button } from "antd";
+import { GoogleOutlined, MailOutlined } from "@ant-design/icons";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-export default function Login() {
-    return (
-        <div>
-           <h1>Login</h1>
+const Login = ({ history }) => {
+  const [email, setEmail] = useState("msulman147@gmail.com");
+  const [password, setPassword] = useState("hassan3459");
+  const [loading, setLoading] = useState(false);
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    if (user && user.token) history.push("/");
+  }, [user]);
+
+  let dispatch = useDispatch();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    // console.table(email, password);
+    try {
+      const result = await auth.signInWithEmailAndPassword(email, password);
+      console.log("result", result);
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+      dispatch({
+        type: "LOGGED_IN_USER",
+        payload: {
+          email: user.email,
+          token: idTokenResult.token,
+        },
+      });
+      history.push("/");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+  const googleLogin = () => {
+    auth
+      .signInWithPopup(googleAuthProvider)
+      .then(async (result) => {
+        const { user } = result;
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error(err.message);
+      });
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <input
+          type="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Your Email"
+          autoFocus
+        />
+      </div>
+      <br />
+      <div className="form-group">
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Your Password"
+        />
+      </div>
+      <br />
+      <Button
+        type="primary"
+        className="mb-3"
+        block
+        onClick={handleSubmit}
+        shape="round"
+        icon={<MailOutlined />}
+        size="large"
+        disabled={!email || password.length < 6}
+      >
+        Login with Email/Password
+      </Button>
+    </form>
+  );
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          {loading ? (
+            <h4 className="text-danger">Loading ....</h4>
+          ) : (
+            <h4>Login</h4>
+          )}
+          {loginForm()}
+          <Button
+            type="danger"
+            className="mb-3"
+            block
+            onClick={googleLogin}
+            shape="round"
+            icon={<GoogleOutlined />}
+            size="large"
+          >
+            Login with Google
+          </Button>
+          <Link to="/forgot/password" className="float-right text-danger">
+            {" "}
+            Forgot Password
+          </Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
+export default Login;
